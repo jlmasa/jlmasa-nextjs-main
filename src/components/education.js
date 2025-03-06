48717abbb5ef31c190a1ed4ea8949a808eed1e52
jlmasa <johnlorenz.masa@gmail.com>
2025-03-06 16:53:16 +0800

    edwq

--- src/components/education.js
+++ src/components/education.js
@@ -5,6 +5,7 @@ import Image from "next/image";
 import { BoxReveal } from "./magicui/box-reveal";
 import { FlipText } from "./magicui/flip-text";
 import { TextAnimate } from "./magicui/text-animate";
+import { BlurFade } from "./magicui/blur-fade";
 const Education = () => {
   return (
     <section>
@@ -23,37 +24,25 @@ const Education = () => {
           <h1 className="lg:text-5xl md:text-2xl text-lg font-bold pt-10">
             I&apos;m John Lorenz Eldon R. Masa
           </h1>
-          <TextAnimate
-            animation="slideUp"
-            by="line"
-            as="h1"
-            className={
-              "lg:text-2xl text-base py-5 lg:w-[60vw] w-[80vw] text-center"
-            }
-          >
-            A graduate of Bachelor of Science in Information Technology. And
-            ever since I first got my own computer unit at the age of 8, I was
-            able to Enjoy a lot of things and It came to a point where I ask
-            myself how does a computer works? How does a computer understand our
-            inputs and that is where I decided to pursue this path of
-            Technology.
-          </TextAnimate>
+          <BlurFade delay={0.25} inView>
+            <h1 className="lg:text-2xl text-base py-5 lg:w-[60vw] w-[80vw] text-center">
+              A graduate of Bachelor of Science in Information Technology. And
+              ever since I first got my own computer unit at the age of 8, I was
+              able to Enjoy a lot of things and It came to a point where I ask
+              myself how does a computer works? How does a computer understand
+              our inputs and that is where I decided to pursue this path of
+              Technology.
+            </h1>
 
-          <TextAnimate
-            animation="slideUp"
-            by="line"
-            as="h1"
-            className={
-              "lg:text-2xl text-base py-5 lg:w-[60vw] w-[80vw] text-center"
-            }
-          >
-            I&apos;m Also a former Member of the student body back in my college
-            years. Where I was Elected As Public Relations Officer back in 2016
-            for a year mainly focus on being the voice of the students. I was
-            also Elected As Vice President of the Student body back in 2017 for
-            a year focus on creating events for the students to enjoy their stay
-            during their college years.
-          </TextAnimate>
+            <h1 className="lg:text-2xl text-base py-5 lg:w-[60vw] w-[80vw] text-center">
+              I&apos;m Also a former Member of the student body back in my
+              college years. Where I was Elected As Public Relations Officer
+              back in 2016 for a year mainly focus on being the voice of the
+              students. I was also Elected As Vice President of the Student body
+              back in 2017 for a year focus on creating events for the students
+              to enjoy their stay during their college years.
+            </h1>
+          </BlurFade>
         </div>
       </div>
     </section>
