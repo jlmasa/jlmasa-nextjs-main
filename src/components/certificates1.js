d2488edc9690eb1c2638169bb97dfa0788fc6a1c
jlmasa <johnlorenz.masa@gmail.com>
2025-03-06 16:34:46 +0800

    added text animate

--- src/components/certificates1.js
+++ src/components/certificates1.js
@@ -4,9 +4,10 @@ import Image from "next/image";
 import cert1 from "../../public/certificate1.webp";
 import cert2 from "../../public/certificate2.webp";
 import Link from "next/link";
-import * as motion from "motion/react-client"
+import * as motion from "motion/react-client";
 import { Lens } from "./magicui/lens";
 import { BoxReveal } from "./magicui/box-reveal";
+
 const Certificates1 = () => {
   return (
     <section>
@@ -14,19 +15,20 @@ const Certificates1 = () => {
         <div className="flex flex-col items-center justify-center">
           <div className="grid lg:grid-cols-2 grid-cols-1 min-h-screen py-10">
             <div className="flex flex-col items-center justify-center  dark:bg-transparent lg:px-0 md:px-20 px-14 ">
-                    <Lens
-                        zoomFactor={2}
-                        lensSize={150}
-                        isStatic={false}
-                        ariaLabel="Zoom Area"
-                      ><Image
-                src={cert1}
-                className="rounded-xl shadow-2xl"
-                alt="image"
-              /></Lens>
-              
+              <Lens
+                zoomFactor={2}
+                lensSize={150}
+                isStatic={false}
+                ariaLabel="Zoom Area"
+              >
+                <Image
+                  src={cert1}
+                  className="rounded-xl shadow-2xl"
+                  alt="image"
+                />
+              </Lens>
             </div>
-            
+
             <div className="flex flex-col items-center justify-center px-20">
               <h1 className="text-3xl font-bold text-amber-400 py-5">
                 HARVARD UNIVERSITY
@@ -35,42 +37,36 @@ const Certificates1 = () => {
                 CS50 : Introduction to Computer Science
               </h1>
               <BoxReveal BoxReveal boxColor={"#fbbf24"} duration={0.3}>
-              <h1 className="lg:text-2xl text-base lg:text-justify text-start">
-                This is CS50, Harvard University&apos;s introduction to the
-                intellectual enterprises of computer science and the art of
-                programming, for concentrators and non-concentrators alike, with
-                or without prior programming experience. (More than half of CS50
-                students have never taken CS before!) This course teaches you
-                how to solve problems, both with and without code, with an
-                emphasis on correctness, design, and style. Topics include
-                computational thinking, abstraction, algorithms, data
-                structures, and computer science more generally.
-              </h1>
+                <h1 className="lg:text-2xl text-base lg:text-justify text-start">
+                  This is CS50, Harvard University&apos;s introduction to the
+                  intellectual enterprises of computer science and the art of
+                  programming, for concentrators and non-concentrators alike,
+                  with or without prior programming experience. (More than half
+                  of CS50 students have never taken CS before!) This course
+                  teaches you how to solve problems, both with and without code,
+                  with an emphasis on correctness, design, and style. Topics
+                  include computational thinking, abstraction, algorithms, data
+                  structures, and computer science more generally.
+                </h1>
               </BoxReveal>
               <BoxReveal BoxReveal boxColor={"#fbbf24"} duration={0.3}>
-              <h1 className="lg:text-2xl text-base lg:text-justify text-start py-10">
-                Students who earn a satisfactory score on 9 problem sets (i.e.,
-                programming assignments) and a final project are eligible for a
-                certificate. This is a self-paced course–you may take CS50x on
-                your own schedule.
-              </h1>
+                <h1 className="lg:text-2xl text-base lg:text-justify text-start py-10">
+                  Students who earn a satisfactory score on 9 problem sets
+                  (i.e., programming assignments) and a final project are
+                  eligible for a certificate. This is a self-paced course–you
+                  may take CS50x on your own schedule.
+                </h1>
               </BoxReveal>
-              
-              <motion.div
-            whileHover={{ scale: 1.2 }}
-            whileTap={{ scale: 0.8 }}
-            
-        >
-           <Link href="https://pll.harvard.edu/course/cs50-introduction-computer-science">
-                <button className="bg-amber-500 hover:bg-amber-700 text-white font-bold py-2 px-4 rounded-full">
-                  Learn More
-                </button>
-              </Link>
-        </motion.div>
-   
+
+              <motion.div whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.8 }}>
+                <Link href="https://pll.harvard.edu/course/cs50-introduction-computer-science">
+                  <button className="bg-amber-500 hover:bg-amber-700 text-white font-bold py-2 px-4 rounded-full">
+                    Learn More
+                  </button>
+                </Link>
+              </motion.div>
             </div>
           </div>
-
         </div>
       </div>
     </section>
--- src/components/school.js
+++ src/components/school.js
@@ -2,41 +2,44 @@
 import React from "react";
 import face1 from "../../public/college.webp";
 import Image from "next/image";
+import { BlurFade } from "./magicui/blur-fade";
 
 const School = () => {
   return (
     <section>
       <div className="flex items-center justify-center min-h-screen">
-        <div className="grid lg:grid-cols-2 grid-cols-1">
-          <div className="flex flex-col items-start justify-center px-20">
-            <strong>
-              <h1 className="text-3xl py-5 text-green-400">EDUCATION</h1>
-            </strong>
+        <BlurFade delay={0.25} inView>
+          <div className="grid lg:grid-cols-2 grid-cols-1">
+            <div className="flex flex-col items-start justify-center px-20">
+              <strong>
+                <h1 className="text-3xl py-5 text-green-400">EDUCATION</h1>
+              </strong>
 
-            <h1 className="lg:text-5xl text-2xl font-bold ">
-              Colegio de San Juan de Letran - Calamba
-            </h1>
-            <h1 className="lg:text-2xl text-lg lg:text-justify text-start py-5">
-              I studied at Colegio de San Juan de Letran-Calamba Located at
-              Bucal Calamba Laguna.
-            </h1>
-            <h1 className="lg:text-2xl text-lg lg:text-justify text-start py-5">
-              Listed below were the courses I took:
-            </h1>
-            <h1 className="lg:text-xl  text-base font-bold pt-5">
-              Bachelor of Science in Entertainment and Multimedia Computing
-              Major in Digital Art
-            </h1>
-            <h1>2015 - 2016</h1>
-            <h1 className="lg:text-xl  text-base font-bold pt-5">
-              Bachelor of Science in Information Technology
-            </h1>
-            <h1>2016 - 2020</h1>
+              <h1 className="lg:text-5xl text-2xl font-bold ">
+                Colegio de San Juan de Letran - Calamba
+              </h1>
+              <h1 className="lg:text-2xl text-lg lg:text-justify text-start py-5">
+                I studied at Colegio de San Juan de Letran-Calamba Located at
+                Bucal Calamba Laguna.
+              </h1>
+              <h1 className="lg:text-2xl text-lg lg:text-justify text-start py-5">
+                Listed below were the courses I took:
+              </h1>
+              <h1 className="lg:text-xl  text-base font-bold pt-5">
+                Bachelor of Science in Entertainment and Multimedia Computing
+                Major in Digital Art
+              </h1>
+              <h1>2015 - 2016</h1>
+              <h1 className="lg:text-xl  text-base font-bold pt-5">
+                Bachelor of Science in Information Technology
+              </h1>
+              <h1>2016 - 2020</h1>
+            </div>
+            <div className="flex flex-col items-center justify-center p-14">
+              <Image src={face1} className="rounded-xl" alt="image" />
+            </div>
           </div>
-          <div className="flex flex-col items-center justify-center p-14">
-            <Image src={face1} className="rounded-xl" alt="image" />
-          </div>
-        </div>
+        </BlurFade>
       </div>
     </section>
   );
